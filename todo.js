26ef497a4ad46e67d677e6b56d240f9700e2e749
arrayslayer <arrayslayer@gmail.com>
2018-05-24 11:55:42 +0530

    Add checkbox to new note added

--- todo.js
+++ todo.js
@@ -1,16 +1,25 @@
 function addNewItem(list,todoText){
 
-  var listItem = document.createElement("li");
-  listItem.innerText = todoText;
+  var listItem = document.createElement("li");// create list element
 
-  list.append(listItem);
+  var textnode = document.createTextNode(todoText);//create text element
+  // by setting attribute innerText of listItem checkbox came after text so did not use
+
+  var inputEle = document.createElement("input");// creating input element
+  inputEle.type="checkbox";//setting its type to a checkbox
+
+  listItem.append(inputEle);//first adding checkbox as child
+  listItem.append(textnode);//now adding text as child
+
+  list.append(listItem);//adding new item to list
+
 
 }
 
 var todoTextElement = document.getElementById('inItemText');// getting DOM
 todoTextElement.focus(); // keeps cursor blinking on input text space intially
 
-todoTextElement.onkeyup = function(e){
+todoTextElement.onkeyup = function(e){ // just like onclick. But onkeyup acts on key press
 
   // capture the event using e. Sent by browser by default
   // Add item only if key pressed is enter. ASCII for enter is 13
