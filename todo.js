71ec2b346775c5f30c30adb27d3fc043ab1f7cd8
arrayslayer <arrayslayer@gmail.com>
2018-05-24 10:43:18 +0530

    Add input text directly

--- todo.js
+++ todo.js
@@ -10,9 +10,13 @@ function addNewItem(list,todoText){
 
 var btnNew = document.getElementById("btnAdd");
 
+var todoTextElement = document.getElementById('inItemText');// getting DOM
+todoTextElement.focus(); // keeps cursor blinking on input text space intially
+
 btnNew.onclick = function(){
 
-  var todoText = prompt("New note");
+  var todoTextElement = document.getElementById('inItemText');
+  var todoText = todoTextElement.value;
 
   if(!todoText || todoText == "")
     return false;
